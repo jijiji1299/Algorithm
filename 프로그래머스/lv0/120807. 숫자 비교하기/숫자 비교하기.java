aa0ce5efed566361d3c249ba851daa7d3b43eed1
김지원 <116232841+jijiji1299@users.noreply.github.com>
2023-07-10 23:10:05 +0900

    Update 숫자 비교하기.java

--- 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
+++ 프로그래머스/lv0/120807. 숫자 비교하기/숫자 비교하기.java
@@ -9,3 +9,11 @@ class Solution {
         return answer;
     }
 }
+
+// 다른 풀이 (삼항연산자)
+class Solution {
+    public int solution(int num1, int num2) {
+        int answer = (num1 == num2) ? 1 : -1;
+        return answer;
+    }
+}
